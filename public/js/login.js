f28cff72bce5f05c6de3558dbf3dc0574371dd77
Dane Enright <enrightco@comcast.net>
2021-06-02 11:42:50 -0600

    created login/logout js

---
+++ public/js/login.js
@@ -0,0 +1,50 @@
+const { response } = require("express");
+
+const loginFormHandler = async (event) => {
+  event.preventDefault();
+
+  const email = document.querySelector("#login-email").value.trim();
+  const password = document.querySelector("#login-password").value.trim();
+
+  if (email && password) {
+    const response = await fetch("/api/users/login", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert("User failed to log in. Please try again!");
+    }
+  }
+};
+
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  const username = document.querySelector("#username").value.trim();
+  const email = document.querySelector("#signup-email").value.trim();
+  const password = document.querySelector("#signup-password");
+
+  if (username && email && password) {
+    const response = await fetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({ username, email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert("User failed to log in. Please try again!");
+    }
+  }
+};
+
+document.querySelector(".login-form");
+document.querySelector("submit", loginFormHandler);
+
+document.querySelector(".signup-form");
+document.querySelector("submit", signupFormHandler);
