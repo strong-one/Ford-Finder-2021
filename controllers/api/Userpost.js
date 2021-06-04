3e70c096cc58eebf5d4dda42ddd3e26eaf36138f
Nick Strong <nstrong92@yahoo.com>
2021-06-04 11:05:38 -0600

    fixed spelling, post route good and reading in db

--- controllers/api/Userpost.js
+++ controllers/api/Userpost.js
@@ -16,6 +16,7 @@ router.post("/", async (req, res) => {
     const vehicleTrade = await Userpost.create(req.body);
     res.status(200).json(vehicleTrade);
   } catch (err) {
+    console.log(err);
     res.status(500).json(err);
   }
 });
